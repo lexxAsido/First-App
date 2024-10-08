ba305d9a77c8737775081e91c02a890f886ac38b
Alexandar Asido <alexandaras2015@gmail.com>
2024-10-08 14:48:55 +0100

    new changes

--- App.js
+++ App.js
@@ -1,22 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, ScrollView, Text, View, StyleSheet, Linking } from 'react-native';
+import Login from './Screens/Login';
 
 export default function App() {
+  
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Alex App is active JavaScript to start working on your app!</Text>
-      
-      <StatusBar style="auto" />
+    <View>
+        <Login/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
---
+++ Screens/Login.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+
+export default function Login(){
+
+    return (
+            <View>
+                <Text>This is just a Login screen</Text>
+            </View>
+    );
+};
